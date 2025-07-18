7eb04ae3e1c263e088d1ac9a1a424a6a3461e2da
Ben Pringle <benjamin.m.pringle@gmail.com>
2025-07-18 16:52:24 -0400

    Add graphics class.

--- ud_matchclient/match.cpp
+++ ud_matchclient/match.cpp
@@ -3,60 +3,31 @@
 #include <emscripten/html5_webgl.h>
 #include <GLES3/gl3.h>
 #include <iostream>
+#include "graphics.h"
+#include "VertexTypes.h"
 
-EMSCRIPTEN_WEBGL_CONTEXT_HANDLE context;
-GLuint vertex_buffer;
+Graphics* graphics;
 
 void game_loop() {
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	graphics->clear_screen();
+	graphics->draw_buffer("triangle");
 }
 
 void graphics_init() {
-	// Initialize WebGL context.
-	EmscriptenWebGLContextAttributes context_attributes;
-	context_attributes.powerPreference = EM_WEBGL_POWER_PREFERENCE_HIGH_PERFORMANCE;
-	context_attributes.majorVersion = 2;
-	context_attributes.minorVersion = 0;
-	context = emscripten_webgl_create_context("#game_canvas", &context_attributes);
-	emscripten_webgl_make_context_current(context);
-
-	// Create vertex buffer.
-	glGenBuffers(1, &vertex_buffer);
-	float vertex_data[6] = {-1, -1, 0, 1, 1, -1};
-	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-	glBufferData(GL_ARRAY_BUFFER, 2 * 3 * sizeof(float), &vertex_data, GL_STATIC_DRAW);
-
-	// Create vertex shader
-	std::string vertex_shader_src = "#version 300 es\nlayout (location = 0) in vec2 pos;\nvoid main() { gl_Position = vec4(pos.x, pos.y, 0, 1.0);} ";
-	int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-	const char* raw_vs_str = vertex_shader_src.data();
-	int vs_length = vertex_shader_src.length();
-	glShaderSource(vertex_shader, 1, &raw_vs_str, &vs_length);
-	glCompileShader(vertex_shader);
-
-	// Create fragment shader
-	std::string fragment_shader_src = "#version 300 es\nprecision mediump float;\nout vec4 color;\nvoid main() { color = vec4(1.0, 1.0, 1.0, 1.0);} ";
-	int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-	const char* raw_fs_str = fragment_shader_src.data();
-	int fs_length = fragment_shader_src.length();
-	glShaderSource(fragment_shader, 1, &raw_fs_str, &fs_length);
-	glCompileShader(fragment_shader);
-
-	// Create, link, and use shader program.
-	int shader_program = glCreateProgram();
-	glAttachShader(shader_program, vertex_shader);
-	glAttachShader(shader_program, fragment_shader);
-	glLinkProgram(shader_program);
-	glUseProgram(shader_program);
-
-	// Delete shaders
-	glDeleteShader(vertex_shader);
-	glDeleteShader(fragment_shader);
-
-	// Bind vertex attribute data.
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
+	graphics = new Graphics("#game_canvas");
+	Vertex2D data [3] = {
+		Vertex2D(-1, -1),
+		Vertex2D(0, 1),
+		Vertex2D(1, -1)
+	};
+	graphics->create_vertex_buffer(
+		"triangle",
+		"#version 300 es\nlayout (location = 0) in vec2 pos;\nvoid main() { gl_Position = vec4(pos.x, pos.y, 0, 1.0);} ",
+		"#version 300 es\nprecision mediump float;\nout vec4 color;\nvoid main() { color = vec4(1.0, 1.0, 1.0, 1.0);} ",
+		data,
+		3,
+		GL_STATIC_DRAW
+	);
 }
 
 extern "C" void EMSCRIPTEN_KEEPALIVE resize_gl_viewport(float width, float height) {
--- ud_matchclient/transpile.sh
+++ ud_matchclient/transpile.sh
@@ -1,4 +1,7 @@
-emcc -O3 *.cpp -std=c++17 -sMODULARIZE=1 -sNO_EXIT_RUNTIME=1 -sMAX_WEBGL_VERSION=2 -sEXPORTED_RUNTIME_METHODS=ccall,cwrap
+em++ -O3 -MJ ../compile_commands_em++.json *.cpp -std=c++20 -s GL_DEBUG=1 -sMODULARIZE=1 -sNO_EXIT_RUNTIME=1 -sMAX_WEBGL_VERSION=2 -sEXPORTED_RUNTIME_METHODS=ccall,cwrap
+echo [ > ../compile_commands.json
+cat ../compile_commands_em++.json >> ../compile_commands.json
+echo ] >> ../compile_commands.json
 
 cp *.js ../ud_webserver/match/static/match/game_files/
 cp *.wasm ../ud_webserver/match/static/match/game_files/
